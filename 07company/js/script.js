4e4835ef2713bbcffe8d6587f186e65678e24137
chaejjji <chaeji3411@naver.com>
2022-07-21 18:20:35 +0900

    0721

--- 07company/js/script.js
+++ 07company/js/script.js
@@ -1,12 +1,13 @@
 console.log("test");
 
+// header_Main
 $(".navbar>ul>li").hover(function(){
     $(this).addClass("on");
 },function(){
     $(this).removeClass("on");
 });
 
-
+// toggle
 $(".toggle").click(function(){
     $(".navbarClone").css("left",0);
     $(".overlay").show();
@@ -15,3 +16,18 @@ $(".overlay").click(function(){
     $(".navbarClone").css("left","-100%");
     $(".overlay").hide();
 });
+
+// acco
+$(".navbarClone>ul>li>a").click(function(e){
+    e.preventDefault();
+
+    $(".navbarClone>ul>li>a").parent().find(".tsubmenu").slideUp();
+    if( $(this).hasClass("active") ){
+        $(".navbarClone>ul>li>a").removeClass("active");
+        
+    }else{
+        $(this).parent().find(".tsubmenu").slideDown();
+        $(".navbarClone>ul>li>a").removeClass("active");
+        $(this).addClass("active");
+    }
+})
