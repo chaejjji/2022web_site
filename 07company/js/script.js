a5292a6816d9fa4b1239d8f947a224a268e52771
chaejjji <chaeji3411@naver.com>
2022-07-29 18:19:29 +0900

    0729

--- 07company/js/script.js
+++ 07company/js/script.js
@@ -51,6 +51,59 @@ var swiper = new Swiper(".mySwiper", {
     },
   });
 
+// #news_slide
+setInterval(slideNum, 3000)
+
+let num = 0;
+function slideNum() {
+    num++;
+    if (num == 4) {
+        num = 1;
+        $(".newsImg").css("left", 0);
+    }
+    $(".newsImg").stop().animate({left: num * -310 }, 500)
+}
+
+// text#1
+setInterval(slideNum1,3000)
+
+let num1 = 0;
+let view1 = 1;
+function slideNum1(){
+    num1++;
+    if ( num1 == $(".slide_Text1>p").length){
+        num1 = 0;
+    }
+    $(".slide_Text1>p").eq(num1).css({"z-index":view1++,opacity:0}).stop().animate({opacity:1},500)
+
+}
+// text#2
+setInterval(slideNum2,3000)
+
+let num2 = 0;
+let view2 = 1;
+function slideNum2(){
+    num2++;
+    if ( num2 == $(".slide_Text2>p").length){
+        num2 = 0;
+    }
+    $(".slide_Text2>p").eq(num2).css({"z-index":view2++,opacity:0}).stop().animate({opacity:1},500)
+
+}
+setInterval(slideNum3,3000)
+
+let num3 = 0;
+let view3 = 1;
+function slideNum3(){
+    num3++;
+    if ( num3 == $(".slide_Text3>p").length){
+        num3 = 0;
+    }
+    $(".slide_Text3>p").eq(num3).css({"z-index":view3++,opacity:0}).stop().animate({opacity:1},500)
+
+}
+
+
 
 
 
